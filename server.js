fa5a28286621e240e5f64bfe8e9316e0584f04d7
Srivar <srivarschool@gmail.com>
2021-10-05 12:43:26 +0530

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 connectDB();
 
-app.get('/', (req, res) => res.send('YES'));
+app.get('/', (req, res) => res.send('SRIVAR'));
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
