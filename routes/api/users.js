49a660ee51755ef81daabbc46f9d9b3d3278e281
Srivar <srivarschool@gmail.com>
2021-10-05 14:24:51 +0530

    first commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post(
+    '/',
+    [
+    check('name', 'Name is required')
+    .not()
+    .isEmpty(),
+    check('email', 'Please include a valid email like example@test.com').isEmail(),
+    check(
+        'password',
+        'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+],
+ (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array});
+    }
+
     res.send('User route');
 });
 
