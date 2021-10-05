ac51a87ca78eb29c795a84dfb769083f3074920f
Srivar <srivarschool@gmail.com>
2021-10-05 14:08:17 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const { body } = require('express-validator');
 const connectDB = require('./config/db');
 
 const app = express();
 
 connectDB();
+
+app.use(express.json({ extended: false }));
+
 
 app.get('/', (req, res) => res.send('SN'));
 
