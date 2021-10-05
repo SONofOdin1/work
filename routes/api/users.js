17c0d2872066ee812764038be942e236c1b7b513
Srivar <srivarschool@gmail.com>
2021-10-05 19:03:00 +0530

    first commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,7 +49,8 @@ router.post(
          password
     });
 
-      const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(15);
+      console.log(salt);  
 
       user.password = await bcrypt.hash(password, salt);
 
