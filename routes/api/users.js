03ac65ac349e1d85996dc0d45e6578f39af94184
Srivar <srivarschool@gmail.com>
2021-10-06 14:34:49 +0530

    first commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
 const { check, validationResult } = require("express-validator");
 
 const User = require('../../models/User');
